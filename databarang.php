8384134adc55d61a54de9948cb40977bc0ba69d6
DestariaSitorus <148428126+DestariaSitorus@users.noreply.github.com>
2024-06-04 15:12:00 +0700

    pengeditan

---
+++ databarang.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="databarang.css">
+</head>
+<body>
+    <div class="container">
+        <div class="table-container">
+            <h3 class="mb-4">Stok Barang</h3>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Nama Barang</th>
+                        <th>Kode Barang</th>
+                        <th>Jumlah Barang</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Motor</td>
+                        <td>KD002</td>
+                        <td>10</td>
+                    </tr>
+                    <tr>
+                        <td>Sepeda</td>
+                        <td>KD003</td>
+                        <td>7</td>
+                    </tr>
+                    <tr>
+                        <td>Mobil</td>
+                        <td>KD004</td>
+                        <td>15</td>
+                    </tr>
+                    <tr>
+                        <td>Becak</td>
+                        <td>KD005</td>
+                        <td>9</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html leng="en">
+  <head>
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+  </head>
+      <body>
+          <header>
+               <h1>Dashboard</h1>
+          </header>
+        
+          <div class="sidebar">
+            <a class="active" href="loginuser.php">Login</a>
+            <a href="registeruser.php">Register</a>
+            <a href="databarang.php">Pendataan Barang</a>
+            <a href="pembelian.php">Pembelian</a>
+            <a href="">Stok Barang</a>
+          </div>
+            <div class="content">
+            </div>
+      <footer>
+        <p>Author: Copyrigt;dazzledesta@2024<br>
+    </footer>
+    </body>
+    </html>
